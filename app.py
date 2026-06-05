1c6b6cbf4f0d51b6953426106541f4ef959bb951
nikhil <nikhiljain.10905@gmail.com>
2026-06-06 01:28:16 +0530

    added port parameter for cloud db

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def get_db_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=os.getenv("DB_PORT", 3306)  # Naya addition!
     )
 
 @app.route('/', methods=['GET'])
